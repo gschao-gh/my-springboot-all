3a492c66fc8243bbba5fbed1da530cf588ea22fd
gschao-gh <gschao@126.com>
2016-11-24 16:14:35 +0800

    创建spring-servlet项目

---
+++ spt-servlet-service/src/main/Java/com/spt/servlet/SpringApplicationBoot.java
@@ -0,0 +1,19 @@
+package com.spt.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gschao
+ * @date 2016/11/24 16:10.
+ */
+@SpringBootApplication
+public class SpringApplicationBoot {
+    /**
+     * 配置启动入口
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApplicationBoot.class);
+    }
+}
